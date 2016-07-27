c69bfd7023a08550c25f1f992754737612118304
Anuj T <anujthakur016@gmail.com>
2016-07-27 16:06:22 +0530

    bug fixed

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -5,10 +5,9 @@ $(document).ready(function() {
 function onDeviceReady() {		
      $('#scan').click( function() 
 		{
-		  alert('hi');
 		  cordova.plugins.barcodeScanner.scan(
 		  function (result) {
-			  alert("We got a barcode\n" +
+			  alert("Code Scanned\n" +
 					"Result: " + result.text + "\n" +
 					"Format: " + result.format + "\n" +
 					"Cancelled: " + result.cancelled);			  
